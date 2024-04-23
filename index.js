33845791935b44c514c6203129ed9a7e47e6b0df
jawadahmed01 <jawadqubist00@gmail.com>
2024-04-23 02:10:02 -0700

    Plindrome

--- index.js
+++ index.js
@@ -13,3 +13,35 @@ btn.addEventListener("click", () => {
   }
   showmsg.textContent = `${inputdata} : ${msg}`
 })
+
+// Min Max
+
+function findMissingNumber(numbers) {
+  numbers.sort((a, b) => a - b)
+  for (let i = 0; i < numbers.length - 1; i++) {
+    if (numbers[i + 1] - numbers[i] !== 1) {
+      return numbers[i] + 1
+    }
+  }
+  return null
+}
+
+console.log(findMissingNumber([11, 12, 14, 15, 16, 18, 19, 20]))
+console.log(findMissingNumber([1, 2, 3, 4, 5, 7]))
+console.log(findMissingNumber([10, 11, 12, 14, 15]))
+
+// Missing Number
+
+function findMissingNumber(numbers) {
+  numbers.sort((a, b) => a - b)
+  for (let i = 0; i < numbers.length - 1; i++) {
+    if (numbers[i + 1] - numbers[i] !== 1) {
+      return numbers[i] + 1
+    }
+  }
+  return null
+}
+
+console.log(findMissingNumber([11, 12, 14, 15, 16, 18, 19, 20]))
+console.log(findMissingNumber([1, 2, 3, 4, 5, 7]))
+console.log(findMissingNumber([10, 11, 12, 14, 15]))
